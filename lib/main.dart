cd724bec5586ca9f17d36eb1e054c5542f563bb6
ANGEL_OMAR_GUERRERO_ORTEGA <a.21308051280366@cbtis128.edu.mx>
2024-05-07 18:02:12 +0000

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,195 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentButton = 0;
+  List<String> buttonList01 = [
+    "https://raw.githubusercontent.com/GuerreroA128/img_iOS/main/flutterFlow/articulo1.png",
+    "https://raw.githubusercontent.com/GuerreroA128/img_iOS/main/flutterFlow/articulo2.png",
+  ];
+
+  List<String> buttonList02 = [
+    "https://raw.githubusercontent.com/DylanLozanoAvelar/Img_IOS/main/FlutterFlowA12/limpiaparabrisas.png",
+    "https://raw.githubusercontent.com/DylanLozanoAvelar/Img_IOS/main/FlutterFlowA12/aditivo-ecologico.png",
+  ];
+
+  List<String> buttonList03 = [
+    "https://raw.githubusercontent.com/GuerreroA128/img_iOS/main/flutterFlow/articulo4.jpg",
+    "https://raw.githubusercontent.com/GuerreroA128/img_iOS/main/flutterFlow/articulo2.png",
+  ];
+
+  List<String> mainList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    mainList = buttonList01;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  color: Color(0xffd2cf0e)),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ))
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0, top: 40.0, bottom: 30.0),
+                child: Text(
+                  "La mejor pagina\nDe compras",
+                  style: TextStyle(color: Colors.white, fontSize: 35),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: mainList.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return RotatedBox(
+                          quarterTurns: 3,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 20.0, left: 20.0, right: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      currentButton = 2;
+                                      mainList = buttonList03;
+                                    });
+                                  },
+                                  child: Text("Flutter",
+                                      style: TextStyle(
+                                          color: currentButton == 2
+                                              ? Colors.amber
+                                              : Colors.black)),
+                                ),
+                                SizedBox(
+                                  width: 100.0,
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      currentButton = 1;
+                                      mainList = buttonList02;
+                                    });
+                                  },
+                                  child: Text("Flutter",
+                                      style: TextStyle(
+                                          color: currentButton == 1
+                                              ? Colors.amber
+                                              : Colors.black)),
+                                ),
+                                SizedBox(
+                                  width: 100.0,
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      currentButton = 0;
+                                      mainList = buttonList01;
+                                    });
+                                  },
+                                  child: Text("Flutter",
+                                      style: TextStyle(
+                                          color: currentButton == 0
+                                              ? Colors.amber
+                                              : Colors.white)),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.all(20.0),
+                                width: 250,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  child: Image.network(mainList[index - 1],
+                                      fit: BoxFit.cover),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Text("Articulos",
+                                  style: TextStyle(fontSize: 26)),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 15, bottom: 25),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.location_on, color: Colors.amber),
+                                  Text("Ciudad Juarez, Chih",
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.grey))
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                    }),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
